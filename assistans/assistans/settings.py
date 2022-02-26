3d85df089006a6cd2e019f09249369286468ea0d
anatol <anatolminor91@inbox.ru>
2022-02-26 12:07:20 +0300

    created docker-compose for localhost

--- assistans/assistans/settings.py
+++ assistans/assistans/settings.py
@@ -21,13 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=e8afg3=8o!#h^9#-b1yp07^$zcsxg&q32s*e)45i3jh_eiq+l'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get("DOCKER_RUN") == True:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    DEBUG = int(os.environ.get("DEBUG", default=0))
+    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+else:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'django-insecure-=e8afg3=8o!#h^9#-b1yp07^$zcsxg&q32s*e)45i3jh_eiq+l'
 
-ALLOWED_HOSTS = ['*']
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
